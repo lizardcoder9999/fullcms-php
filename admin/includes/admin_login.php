2fc87557cc4acdcd9e39c9b56dded316bc1414d7
lizardcoder9999 <lizardcoder6@gmail.com>
2020-11-24 13:26:33 -0500

    Admin login system is now fully functional I will be adding reset password features for users and admins which will send them a reset email but for now I will be working on more features inside of the cms system

--- admin/includes/admin_login.php
+++ admin/includes/admin_login.php
@@ -1,58 +1,39 @@
-<?php include "db.php"; ?>
-<?php session_start();?>
-
-<?php
-
-if(isset($_POST['login'])){
-
-  $email = $_POST['email'];
-  $password = $_POST['password'];
-
-if(empty($_POST['email']) || empty($_POST['password'])){
-
-  header("Location: ../login.php?error=all_fields_required");
-
-}else{
-
-$query = "SELECT * FROM admins WHERE admin_email = ? AND admin_password = ?";
-$stmt = mysqli_stmt_init($connection);
-if(!mysqli_stmt_prepare($stmt,$query)){
-
-  die("QUERY FAILED".mysqli_error($connection));
-
-}else{
-
-mysqli_stmt_bind_param($stmt,"ss",$email,$password);
-mysqli_stmt_execute($stmt);
-$result = mysqli_stmt_get_result($stmt);
-
-if(mysqli_fetch_assoc($result))
+<?php 
+session_start();
+include_once("db.php");
+if (isset($_POST['login'])) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $query = "SELECT * FROM admins where admin_email = ?";
+    $stmt = mysqli_stmt_init($connection);
+    if (!mysqli_stmt_prepare($stmt, $query)) {
+        die("Query Failed". mysqli_error($connection));
+    } else {
+        mysqli_stmt_bind_param($stmt, 's', $email);
+        mysqli_stmt_execute($stmt);
+        $result = mysqli_stmt_get_result($stmt);
 
 
-  $_SESSION['admin_email'] = $email;
-  header("Location: ../index.php");
 
+
+        while ($row = mysqli_fetch_array($result)) {
+            $db_admin_id = $row['admin_id'];
+            $db_admin_email = $row['admin_email'];
+            $db_admin_name = $row['admin_name'];
+            $db_admin_password = $row['admin_password'];
+            
+                
+            
+            if (password_verify($password, $db_admin_password)) {
+                $_SESSION['admin_id'] = $db_admin_id;
+                $_SESSION['admin_email'] = $db_admin_email;
+                $_SESSION['admin_name'] = $db_admin_name;
+                header("Location: ../index.php");
+            }else{
+              header("Location: ../login.php?error=wrong_email_or_pass");
+            }
+        }
+    }
 }
-
-
-}
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 ?>
