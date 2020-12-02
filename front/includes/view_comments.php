c87ae172409935859ca08189ac31e6a3599d68f8
lizardcoder9999 <lizardcoder6@gmail.com>
2020-12-02 13:01:26 -0500

    Added view comments on the post.php and fixed small bug in view_comments.php and I will be adding specific comments for specific posts

--- front/includes/view_comments.php
+++ front/includes/view_comments.php
@@ -8,12 +8,12 @@ $result = mysqli_query($connection,$query);
 while($row = mysqli_fetch_assoc($result)){
 
 $comment_author = $row['comment_author'];
-$comment_content = $row['comment_text'];
+$comment_content = $row['comment_content'];
 $comment_timestamp = $row['comment_timestamp'];
 
 echo "<div class='media-body'>";
 echo "<h4 class='media-heading'>$comment_author";
-echo  "<small>$comment_timestamp</small>";
+echo  "<small> $comment_timestamp</small>";
 echo "</h4>";
 echo "$comment_content";
 echo "</div>";
--- front/post.php
+++ front/post.php
@@ -120,20 +120,11 @@
                 <!-- Posted Comments -->
 
                 <!-- Comment -->
-                <div class="media">
-                    <a class="pull-left" href="#">
-                        <img class="media-object" src="http://placehold.it/64x64" alt="">
-                    </a>
-                    <div class="media-body">
-                        <h4 class="media-heading">Start Bootstrap
-                            <small>August 25, 2014 at 9:30 PM</small>
-                        </h4>
-                        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                    </div>
-                </div>
-
+                <?php 
+                include("./includes/view_comments.php");
+                ?>
                 <!-- Comment -->
-                <div class="media">
+                <!-- <div class="media">
                     <a class="pull-left" href="#">
                         <img class="media-object" src="http://placehold.it/64x64" alt="">
                     </a>
@@ -143,7 +134,7 @@
                         </h4>
                         Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
                         <!-- Nested Comment -->
-                        <div class="media">
+                        <!-- <div class="media">
                             <a class="pull-left" href="#">
                                 <img class="media-object" src="http://placehold.it/64x64" alt="">
                             </a>
@@ -153,7 +144,7 @@
                                 </h4>
                                 Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
                             </div>
-                        </div>
+                        </div> --> -->
                         <!-- End Nested Comment -->
                     </div>
                 </div>
