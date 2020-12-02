f2d6a89705535f0948b9dc786d14ba0a05492f61
lizardcoder9999 <lizardcoder6@gmail.com>
2020-12-02 09:36:59 -0500

    Added view_comments.php going to implement it to the post page soon

---
+++ front/includes/view_comments.php
@@ -0,0 +1,33 @@
+<?php 
+
+include_once("db.php");
+
+$query = "SELECT * FROM comments";
+$result = mysqli_query($connection,$query);
+
+while($row = mysqli_fetch_assoc($result)){
+
+$comment_author = $row['comment_author'];
+$comment_content = $row['comment_text'];
+$comment_timestamp = $row['comment_timestamp'];
+
+echo "<div class='media-body'>";
+echo "<h4 class='media-heading'>$comment_author";
+echo  "<small>$comment_timestamp</small>";
+echo "</h4>";
+echo "$comment_content";
+echo "</div>";
+echo "</div>";
+
+}
+
+
+
+
+
+
+
+
+
+
+?>
